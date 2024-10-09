4a99dee75be695c03eb5978ef759e07bf45ee69c
Parsa-D86 <parsadalfardy@gmail.com>
2024-10-09 19:43:59 +0330

    Update main.cpp
    added user choice functionality on the matter of clicks produced following each real click

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 atomic<bool> isSimulatingClick (false);
 atomic<bool> isLeftClickActive (false);
 atomic<bool> isRightClickActive(false);
+atomic<int>  numExtraClicks(2); //defaults to 2
 
 inline int AntiDetectionSolution(){
     static default_random_engine generator;
@@ -17,8 +18,8 @@ inline int AntiDetectionSolution(){
     return max(10, min(60, delay));
 }
 
-void SimulateClick(DWORD clickFlagDown, DWORD clickFlagUp) {
-    INPUT input = { 0 };
+void SimulateClick(DWORD clickFlagDown, DWORD clickFlagUp){
+    INPUT input = {0};
     input.type = INPUT_MOUSE;
     input.mi.dwFlags = clickFlagDown;
     SendInput(1, &input, sizeof(INPUT));
@@ -29,23 +30,22 @@ void SimulateClick(DWORD clickFlagDown, DWORD clickFlagUp) {
     SendInput(1, &input, sizeof(INPUT));
 }
 
-LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
-    if (nCode >= 0 && wParam == WM_LBUTTONDOWN && isLeftClickActive) {
+LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam){
+    if (nCode >= 0 && wParam == WM_LBUTTONDOWN && isLeftClickActive){
         MSLLHOOKSTRUCT* mouseStruct = (MSLLHOOKSTRUCT*)lParam;
 
-        if (!isSimulatingClick && mouseStruct != nullptr) {
-            cout << "Left click detected! Simulating..." << std::endl;
+        if (!isSimulatingClick && mouseStruct != nullptr){
+            cout<<"Left click detected! Simulating..."<<std::endl;
 
-           thread([]() {
+            thread([]() {
                 isSimulatingClick = true;
-                int numExtraClicks = 2;
 
-                for (int i = 0; i < numExtraClicks; ++i) {
+                for (int i = 0; i < numExtraClicks; ++i){
                     this_thread::sleep_for(chrono::milliseconds(AntiDetectionSolution()));
                     SimulateClick(MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP);
                 }
 
-                cout << "Left clicks simulated." << std::endl;
+                cout<<"Left clicks simulated."<<std::endl;
                 isSimulatingClick = false;
             }).detach();
         }
@@ -54,19 +54,18 @@ LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode >= 0 && wParam == WM_RBUTTONDOWN && isRightClickActive) {
         MSLLHOOKSTRUCT* mouseStruct = (MSLLHOOKSTRUCT*)lParam;
 
-        if (!isSimulatingClick && mouseStruct != nullptr) {
-            cout << "Right click detected! Simulating..." << endl;
+        if (!isSimulatingClick && mouseStruct != nullptr){
+            cout<<"Right click detected! Simulating..."<<endl;
 
-            thread([]() {
+            thread([](){
                 isSimulatingClick = true;
-                int numExtraClicks = 2;
 
-                for (int i = 0; i < numExtraClicks; ++i) {
+                for (int i = 0; i < numExtraClicks; ++i){
                     this_thread::sleep_for(chrono::milliseconds(AntiDetectionSolution()));
                     SimulateClick(MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP);
                 }
 
-                cout << "Right clicks simulated." << endl;
+                cout<<"Right clicks simulated."<<endl;
                 isSimulatingClick = false;
             }).detach();
         }
@@ -75,35 +74,46 @@ LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-int main(){
+int main() {
+    int userChoice = 0;
+    while(userChoice < 1 || userChoice > 2){
+        cout<<"how many extra clicks?(1 or 2): ";
+        cin>>userChoice;
+        if(userChoice >= 1 && userChoice <= 2){
+            numExtraClicks = userChoice;
+        }else {
+            cout << "Invalid. choose between 1 and 2." << endl;
+        }
+    }
+
     HHOOK mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookProc, NULL, 0);
-    if (mouseHook == NULL){
-        cerr<<"Failed to hook!" <<endl;
+    if (mouseHook == NULL) {
+        cerr << "Failed to hook!" << endl;
         return 1;
     }
 
-    cout<<"Hooked..."<<endl;
+    cout << "Hooked..." << endl;
 
     thread([]() {
-        while(true){
-            if(GetAsyncKeyState(VK_XBUTTON2) & 0x8000){ 
+        while (true) {
+            if (GetAsyncKeyState(VK_XBUTTON2) & 0x8000){
                 isLeftClickActive = !isLeftClickActive;
-                cout<<(isLeftClickActive ? "Left click simulation ON" : "Left click simulation OFF")<<endl;
+                cout << (isLeftClickActive ? "Left click simulation ON" : "Left click simulation OFF")<< endl;
                 this_thread::sleep_for(chrono::milliseconds(300));
             }
 
-            if(GetAsyncKeyState(VK_XBUTTON1) & 0x8000){
+            if (GetAsyncKeyState(VK_XBUTTON1) & 0x8000){
                 isRightClickActive = !isRightClickActive;
-                cout<<(isRightClickActive ? "Right click simulation ON" : "Right click simulation OFF")<<endl;
+                cout<<(isRightClickActive ? "Right click simulation ON" : "Right click simulation OFF")<< endl;
                 this_thread::sleep_for(chrono::milliseconds(300));
             }
-			
+
             this_thread::sleep_for(chrono::milliseconds(100));
         }
     }).detach();
 
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0)){
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
